b2e178dde4c5cdd1b2f78a80e0dbd3f6cc4559aa
vieirawilliam <willian_43@hotmail.com>
2019-10-21 10:46:53 -0300

    Teste de push

---
+++ switch/exemplo1.php
@@ -0,0 +1,44 @@
+<?php
+
+$diaDaSemana = 8;
+
+echo $diaDaSemana;
+
+switch ($diaDaSemana) {
+        case '0':
+        echo "Domingo";
+        break;
+    
+        case '1':
+        echo "Segunda";
+        break;
+
+        case '2':
+        echo "Terça";
+        break;
+
+        case '2':
+        echo "Terça";
+        break;
+
+        case '3':
+        echo "Quarta";
+        break;
+
+        case '4':
+        echo "Quinta";
+        break;
+
+        case '5':
+        echo "Sexta";
+        break;
+
+        case '6':
+        echo "Sabado";
+        break;
+    default:
+        echo "Data invalida";
+        break;
+}
+
+?>
